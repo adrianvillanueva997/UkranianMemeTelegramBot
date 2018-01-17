8eeee81a5be6d41e147a427947a373b4353e35aa
Adrián Villanueva <thexiao77@users.noreply.github.com>
2018-01-17 18:56:51 +0100

    added more fun functions

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+import urllib
+from urllib import request
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Job
 import logging
 import requests
@@ -9,11 +11,12 @@ import re
 from random import randint
 from lib import py8chan
 
+
 hostname = ''
 user = ''
 password = ''
 database = ''
-port = 1
+port = 3306
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -133,13 +136,12 @@ def randomThread(bot, update, args):
             bot.send_message(chat_id=update.message.chat_id, text=(f.file_url))
             # print(' Fileurl', f.file_url)
             break
-        bot.send_message(chat_id=update.message.chat_id, text=(
-            'http://boards.4chan.org/' + str(query) + '/thread/' + str(threadIds[randomThread])))
+        update.message.reply_text('http://boards.4chan.org/' + str(query) + '/thread/' + str(threadIds[randomThread]))
 
     except Exception as exception:
         print(exception)
         bot.send_message(chat_id=update.message.chat_id,
-                         text='Board not found')
+                         text=('board not found'))
 
 
 def randomBoard(bot, update):
@@ -172,12 +174,12 @@ def randomBoard(bot, update):
             # print(' Fileurl', f.file_url)
             break
         bot.send_message(chat_id=update.message.chat_id, text=(
-            'http://boards.4chan.org/' + str(randomBoard) + '/thread/' + str(threadIds[randomThread])))
+                'http://boards.4chan.org/' + str(randomBoard) + '/thread/' + str(threadIds[randomThread])))
 
     except Exception as exception:
         print(exception)
         bot.send_message(chat_id=update.message.chat_id,
-                         text=exception)
+                         text=('Error'))
 
 
 def weather(bot, update, args):
@@ -186,7 +188,7 @@ def weather(bot, update, args):
         cities = []
         cities.append(city)
         base_url = 'http://api.openweathermap.org/data/2.5/weather'
-        api_key = '5991f4bcb0884409212e0e4d6ff2c60c'  # << Get your API key (APPID) here: http://openweathermap.org/appid
+        api_key = ''  # << Get your API key (APPID) here: http://openweathermap.org/appid
         query = base_url + '?q=%s&units=metric&APPID=%s' % (city, api_key)
         try:
             response = requests.get(query)
@@ -197,19 +199,19 @@ def weather(bot, update, args):
                 weather_data = response.json()
                 for city in cities:
                     location = weather_data
-                    bot.send_message(chat_id=update.message.chat_id, text=(
-                        'City: ' + str(location['name'] +
-                                       '\nTemperature: ' + str(location['main']['temp']) + 'ºC' +
-                                       '\nHumidity: ' + str(location['main']['humidity']) + '%' +
-                                       '\nPressure: ' + str(location['main']['pressure']) + 'hPA' +
-                                       '\nClouds: ' + str(location['clouds']['all']) + ' %')))
+                    update.message.reply_text('City: ' + str(location['name'] +
+                                                             '\nTemperature: ' + str(location['main']['temp']) + 'ºC' +
+                                                             '\nHumidity: ' + str(location['main']['humidity']) + '%' +
+                                                             '\nPressure: ' + str(
+                        location['main']['pressure']) + 'hPA' +
+                                                             '\nClouds: ' + str(location['clouds']['all']) + ' %'))
         except Exception as exception:
 
             bot.send_message(chat_id=update.message.chat_id,
-                             text=exception)
+                             text=str(exception))
     except requests.exceptions.RequestException as error:
         bot.send_message(chat_id=update.message.chat_id,
-                         text=error)
+                         text=str(error))
 
 
 def wikiRed(bot, update):
@@ -232,7 +234,7 @@ def wikiRed(bot, update):
         con.close()
     except Exception as exception:
         bot.send_message(chat_id=update.message.chat_id,
-                         text=exception)
+                         text=str(exception))
 
 
 def getShit():
@@ -283,7 +285,8 @@ def shitPost(bot, update):
         bot.send_message(chat_id=update.message.chat_id,
                          text=nuevoTest)
     except Exception as e:
-        print(e)
+        bot.send_message(chat_id=update.message.chat_id,
+                         text=str(e))
 
 
 def random8ChanBoard(bot, update):
@@ -317,12 +320,13 @@ def random8ChanBoard(bot, update):
 
         bot.send_message(chat_id=update.message.chat_id,
                          text=(
-                             'https://8ch.net/' + str(randomBoard) + '/res/' + str(threadIds[randomThread]) + '.html'))
+                                 'https://8ch.net/' + str(randomBoard) + '/res/' + str(
+                             threadIds[randomThread]) + '.html'))
 
     except Exception as exception:
         print(exception)
         bot.send_message(chat_id=update.message.chat_id,
-                         text='Board not found')
+                         text=str(exception))
 
 
 def random8ChanThread(bot, update, args):
@@ -352,12 +356,12 @@ def random8ChanThread(bot, update, args):
 
         bot.send_message(chat_id=update.message.chat_id,
                          text=(
-                             'https://8ch.net/' + str(query) + '/res/' + str(threadIds[randomThread]) + '.html'))
+                                 'https://8ch.net/' + str(query) + '/res/' + str(threadIds[randomThread]) + '.html'))
 
     except Exception as exception:
         print(exception)
         bot.send_message(chat_id=update.message.chat_id,
-                         text='Board not found')
+                         text=str(exception))
 
 
 def list8ChanBoards(bot, update):
@@ -366,6 +370,8 @@ def list8ChanBoards(bot, update):
                          text=chan8Boards)
     except Exception as e:
         print(e)
+        bot.send_message(chat_id=update.message.chat_id,
+                         text=str(e))
 
 
 def list4ChanBoards(bot, update):
@@ -373,11 +379,74 @@ def list4ChanBoards(bot, update):
         bot.send_message(chat_id=update.message.chat_id,
                          text=boards)
     except Exception as e:
+        bot.send_message(chat_id=update.message.chat_id,
+                         text=str(e))
+
+
+def getRawHTML(url):
+    try:
+
+        headers = {}
+        headers[
+            'User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
+        req = urllib.request.Request(url, headers=headers)
+        resp = urllib.request.urlopen(req)
+        respData = str(resp.read())
+        return respData
+    except Exception as e:
         print(e)
 
 
+def getLinks(html):
+    urls = []
+    url = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', html)
+    print(urls)
+    for link in url:
+        if (str(link).__contains__('encrypted') or str(link).__contains__('google')):
+            print('no')
+        else:
+            print(link)
+            urls.append(link)
+    return urls
+
+
+def getRandomPicture(images):
+    try:
+        randomPic = randint(0, (len(images) - 1))
+        print(randomPic)
+        if (images[randomPic] == None):
+            getRandomPicture(images)
+        else:
+            return images[randomPic]
+    except Exception as e:
+        print(e)
+
+
+def searchImage(bot, update, args):
+    try:
+
+        if (args != None):
+            commandQuery = ' '.join(str(args))
+            query = commandQuery.replace(' ', '%20').encode('utf-8')
+            url = 'https://www.google.com/search?q=' + str(
+                query) + '&espv=2&biw=1366&bih=667&site=webhp&source=lnms&tbm=isch&sa=X&ei=XosDVaCXD8TasATItgE&ved=0CAcQ_AUoAg'
+            html = getRawHTML(url)
+            getLinks(html)
+            urls = getLinks(html)
+            picture = getRandomPicture(urls)
+            update.message.reply_text(picture)
+
+        else:
+            bot.send_message(chat_id=update.message.chat_id, text='no')
+
+    except Exception as e:
+        print(e)
+        bot.send_message(chat_id=update.message.chat_id,
+                         text=str(e))
+
+
 def main():
-    updater = Updater("?????????????????????????????????")
+    updater = Updater("368625073:AAHVfNuLhlW-z3SPC40Cd9Rq9MKI3w8dEv4")
 
     dp = updater.dispatcher
     start_handler = CommandHandler('start', start)
@@ -394,6 +463,7 @@ def main():
     dp.add_handler(CommandHandler("8chanThread", random8ChanThread, pass_args=True))
     dp.add_handler(CommandHandler("4chanboards", list4ChanBoards))
     dp.add_handler(CommandHandler("8chanboards", list8ChanBoards, ))
+    dp.add_handler(CommandHandler("get", searchImage, pass_args=True, pass_user_data=updater.last_update_id))
 
     dp.add_error_handler(error)
 
