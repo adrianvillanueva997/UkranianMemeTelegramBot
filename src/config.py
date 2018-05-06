14a41f13fa26136e0878924ba3fdf09a536640f2
Adrián Villanueva <thexiao77@users.noreply.github.com>
2018-05-06 16:05:38 +0200

    Update config.py

--- src/config.py
+++ src/config.py
@@ -10,7 +10,7 @@ GOOGLE_MAPS_API_URL = 'http://maps.googleapis.com/maps/api/geocode/json'
 GOOGLE_MAPS_API_KEY = ''
 OPbase_url = 'http://api.openweathermap.org/data/2.5/weather'
 OPapi_key = ''  # << Get your API key (APPID) here: http://openweathermap.org/appid
-updater = "368625073:"
+updater = ":"
 dotaApi = ''
 
 timezonekey = ''
